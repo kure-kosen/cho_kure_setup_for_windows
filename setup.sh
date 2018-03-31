7f4d6a307401d8b6077610d3f435b498140aa9c7
srpkdyy <leader931334@outlook.jp>
2018-03-31 18:39:21 +0900

    complete!!!!!Yeah!!!!!!

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 sudo apt update
-sudo apt install -y software-properties-common
-sudo apt install -y build-essential
+sudo apt install -y software-properties-common build-essential libssl-dev libreadline-dev
+
 
 sudo add-apt-repository ppa:git-core/ppa
-sudo apt update
-sudo apt install -y git
-
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
@@ -15,20 +12,16 @@ source $HOME/.bash_profile
 mkdir -p "$(rbenv root)"/plugins
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
-rbenv install 2.4.2
-rbenv global 2.4.2
-gem install bundler
-
 sudo curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 sudo sh -c "echo deb https://deb.nodesource.com/node_8.x zesty main \ > /etc/apt/sources.list.d/nodesource.list"
-sudo apt update
-sudo apt install -y nodejs
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+
 sudo apt update
-sudo apt install -y yarn
+sudo apt install -y git nodejs yarn mysql-server libmysqlclient-dev
 
-sudo apt install -y libmysqlclient-dev
-sudo apt install -y mysql-server
-
+rbenv install 2.4.2
+rbenv global 2.4.2
+gem install bundler
